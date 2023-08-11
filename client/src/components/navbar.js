bbb663a488a77491d9f642d21a2b93f76ebb5af3
JIAHLEE0213 <ja7943@naver.com>
2023-08-11 11:19:57 +0900

    sidebar html 완성

--- client/src/components/navbar.js
+++ client/src/components/navbar.js
@@ -4,35 +4,87 @@ import {
   faStar,
   faBagShopping,
 } from '@fortawesome/free-solid-svg-icons';
+import { styled } from 'styled-components';
+
+const NavbarContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  height: 400px;
+  border: 1px solid black;
+  width: 164px;
+  margin-right: 0.5rem;
+  font-size: 13px;
+  .navbar_container {
+    padding-right: 0.5rem;
+  }
+  .navbar_public {
+    margin-left: 0.2rem;
+  }
+  .navbar_home {
+    margin-left: 0.2rem;
+  }
+  .navbar_public_Questions {
+    display: flex;
+    align-items: center;
+  }
+  .icon_Questions {
+    margin-right: 0.5rem;
+  }
+  .icon_Explore {
+    margin-right: 0.5rem;
+  }
+  .icon_Create {
+    margin-right: 0.5rem;
+  }
+  .navbar_collective_category {
+    display: flex;
+    align-items: center;
+  }
+  .navbar_teams_category {
+    display: flex;
+    align-items: center;
+  }
+  .li {
+    margin-left: 2rem;
+  }
+`;
 
 export default function Navbar() {
   return (
-    <div className="navbar_container">
-      <div className="navbar_home">Home</div>
-      <div className="navbar_public">
-        PUBLIC
-        <ul>
-          <li>
-            <FontAwesomeIcon
-              icon={faEarthAmericas}
-              className="icon_Questions"
-            />
-            <div>Questions</div>
-          </li>
-          <li>Tags</li>
-          <li>Users</li>
-          <li>Companies</li>
-        </ul>
-      </div>
-      <div className="navbar_collective">
-        <FontAwesomeIcon icon={faStar} className="icon_Explore" />
-        <div>Explore Explore Collectives</div>
-      </div>
-      <div className="navbar_teams">
-        <FontAwesomeIcon icon={faBagShopping} className="icon_Creat" />
-        <div>Create free Team</div>
+    <NavbarContainer>
+      <div className="navbar_container">
+        <div className="navbar_home">Home</div>
+        <div className="navbar_public">
+          PUBLIC
+          <ul className="navbar_publi_category">
+            <li className="navbar_public_Questions">
+              <FontAwesomeIcon
+                icon={faEarthAmericas}
+                className="icon_Questions"
+              />
+              <div>Questions</div>
+            </li>
+            <li>Tags</li>
+            <li>Users</li>
+            <li>Companies</li>
+          </ul>
+        </div>
+        <div className="navbar_collective">
+          COLLECTIVES
+          <div className="navbar_collective_category">
+            <FontAwesomeIcon icon={faStar} className="icon_Explore" />
+            <div> Explore Collectives</div>
+          </div>
+        </div>
+        <div className="navbar_teams">
+          TEAMS
+          <div className="navbar_teams_category">
+            <FontAwesomeIcon icon={faBagShopping} className="icon_Create" />
+            <div>Create free Team</div>
+          </div>
+        </div>
         <div className="navbar_">Looking for your Teams?</div>
       </div>
-    </div>
+    </NavbarContainer>
   );
 }
--- client/src/components/sidebar.js
+++ client/src/components/sidebar.js
@@ -1,61 +1,99 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { styled } from 'styled-components';
+import { faMessage } from '@fortawesome/free-regular-svg-icons';
+import { faPencil } from '@fortawesome/free-solid-svg-icons';
+import { faStackOverflow } from '@fortawesome/free-brands-svg-icons';
 
+const SidebarContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  height: 500px;
+  border: 1px solid black;
+  width: 200px;
+  margin-right: 0.5rem;
+  font-size: 13px;
+  .sidebar_widget_firstitem1 {
+    display: flex;
+    margin-right: 0.5rem;
+  }
+  .sidebar_widget_firstitem2 {
+    display: flex;
+  }
+  .sidebar_widget_seconditem1 {
+    display: flex;
+  }
+  .sidebar_widget_seconditem2 {
+    display: flex;
+  }
+  .sidebar_widget_seconditem3 {
+    display: flex;
+  }
+  .sidebar_widget_seconditem4 {
+    display: flex;
+  }
+  .sidebar_widget_seconditem5 {
+    display: flex;
+  }
+`;
 export default function Sidebar() {
   return (
-    <div className="sidebar_container">
-      <ul className="sidebar_widget">
-        <li className="sidebar_widget_first">The Overflow Blog</li>
-        <li className="sidebar_widget_firstitem1">
-          <FontAwesomeIcon />
-          <div className="sidebar_widget_firstitem1_text">
-            Speeding up the I/O-heavy app: Q&A with Malte Ubl of Vercel
-          </div>
-        </li>
-        <li className="sidebar_widget_firstitem2">
-          <FontAwesomeIcon />
-          <div className="sidebar_widget_firstitem2_text">
-            Understanding SRE (Ep. 597)
-          </div>
-        </li>
-        <li className="sidebar_widget_second">Featured on Meata</li>
-        <li className="sidebar_widget_seconditem1">
-          <FontAwesomeIcon />
-          <div className="sidebar_widget_seconditem1_text">
-            Moderation strike: Results of negotiations
-          </div>
-        </li>
-        <li className="sidebar_widget_seconditem2">
-          <FontAwesomeIcon />
-          <div className="sidebar_widget_seconditem2_text">
-            Our Design Vision for Stack Overflow and the Stack Exchange network
-          </div>
-        </li>
-        <li className="sidebar_widget_seconditem3">
-          <FontAwesomeIcon />
-          <div className="sidebar_widget_seconditem3_text">
-            Temporary policy: Generative AI (e.g.,ChatGPT) is banned
-          </div>
-        </li>
-        <li className="sidebar_widget_seconditem4">
-          <FontAwesomeIcon />
-          <div className="sidebar_widget_seconditem4_text">
-            Preview of SEarch and Question-Asking Powered by GenAI
-          </div>
-        </li>
-        <li className="sidebar_widget_seconditem5">
-          <FontAwesomeIcon />
-          <div className="sidebar_widget_seconditem5_text">
-            Collections: A New Feature for Collectives on Stack Overflow
-          </div>
-        </li>
-        <li className="sidebar_widget_third">Hot Meta Posts</li>
-        <li className="sidebar_widget_thirditem1">
-          <FontAwesomeIcon />
-          <div className="sidebar_widget_thirditem1_text">
-            Will Stack Overflox ban the GPT web crawler?
-          </div>
-        </li>
-      </ul>
-    </div>
+    <SidebarContainer>
+      <div className="sidebar_container">
+        <ul className="sidebar_widget">
+          <li className="sidebar_widget_first">The Overflow Blog</li>
+          <li className="sidebar_widget_firstitem1">
+            <FontAwesomeIcon icon={faPencil} />
+            <div className="sidebar_widget_firstitem1_text">
+              Speeding up the I/O-heavy app: Q&A with Malte Ubl of Vercel
+            </div>
+          </li>
+          <li className="sidebar_widget_firstitem2">
+            <FontAwesomeIcon icon={faPencil} />
+            <div className="sidebar_widget_firstitem2_text">
+              Understanding SRE (Ep. 597)
+            </div>
+          </li>
+          <li className="sidebar_widget_second">Featured on Meata</li>
+          <li className="sidebar_widget_seconditem1">
+            <FontAwesomeIcon icon={faMessage} />
+            <div className="sidebar_widget_seconditem1_text">
+              Moderation strike: Results of negotiations
+            </div>
+          </li>
+          <li className="sidebar_widget_seconditem2">
+            <FontAwesomeIcon icon={faMessage} />
+            <div className="sidebar_widget_seconditem2_text">
+              Our Design Vision for Stack Overflow and the Stack Exchange
+              network
+            </div>
+          </li>
+          <li className="sidebar_widget_seconditem3">
+            <FontAwesomeIcon icon={faStackOverflow} />
+            <div className="sidebar_widget_seconditem3_text">
+              Temporary policy: Generative AI (e.g.,ChatGPT) is banned
+            </div>
+          </li>
+          <li className="sidebar_widget_seconditem4">
+            <FontAwesomeIcon icon={faStackOverflow} />
+            <div className="sidebar_widget_seconditem4_text">
+              Preview of SEarch and Question-Asking Powered by GenAI
+            </div>
+          </li>
+          <li className="sidebar_widget_seconditem5">
+            <FontAwesomeIcon icon={faStackOverflow} />
+            <div className="sidebar_widget_seconditem5_text">
+              Collections: A New Feature for Collectives on Stack Overflow
+            </div>
+          </li>
+          <li className="sidebar_widget_third">Hot Meta Posts</li>
+          <li className="sidebar_widget_thirditem1">
+            <FontAwesomeIcon />
+            <div className="sidebar_widget_thirditem1_text">
+              Will Stack Overflox ban the GPT web crawler?
+            </div>
+          </li>
+        </ul>
+      </div>
+    </SidebarContainer>
   );
 }
