06dd1eda35edccb65e68e766624631e1831ac3e3
27Lia <tjs4114@gmail.com>
2023-08-12 04:59:19 +0900

    버튼 비활성화 기능 구현중

--- client/src/components/Editor5.js
+++ client/src/components/Editor5.js
@@ -2,6 +2,7 @@ import { CKEditor } from '@ckeditor/ckeditor5-react';
 import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
 import { styled } from 'styled-components';
 import PropTypes from 'prop-types';
+import { useState } from 'react';
 
 const StyledEditor = styled.div`
   max-width: 800px;
@@ -18,7 +19,7 @@ const StyledEditor = styled.div`
     margin-bottom: 5px;
   }
 
-  button {
+  .next-button {
     height: 36px;
     width: 50px;
     padding: 10px;
@@ -26,6 +27,12 @@ const StyledEditor = styled.div`
     background-color: #0a95ff;
     border-radius: 5px;
     margin-top: 16px;
+    cursor: ${(props) => (props.isButtonDisabled ? 'not-allowed' : 'pointer')};
+
+    /* 비활성화된 상태일 때 배경색 변경 */
+    &:disabled {
+      background-color: #d5d9dc;
+    }
   }
 
   .ck-editor__editable {
@@ -37,8 +44,15 @@ const StyledEditor = styled.div`
 `;
 
 function Editor5({ title, explanation, isButtonVisible, handleButtonClick }) {
+  const [editorContent, setEditorContent] = useState(''); // 텍스트 내용을 상태로 관리
+
+  const handleEditorChange = (event, editor) => {
+    const data = editor.getData();
+    setEditorContent(data); // 텍스트 내용 업데이트
+  };
+
   return (
-    <StyledEditor>
+    <StyledEditor isButtonDisabled={editorContent.length < 20}>
       <div className="editor-container">
         <div>
           <div className="title-box">
@@ -50,6 +64,7 @@ function Editor5({ title, explanation, isButtonVisible, handleButtonClick }) {
         <CKEditor
           editor={ClassicEditor}
           data=""
+          onChange={handleEditorChange} // 텍스트 변경 시 호출되는 함수
           onReady={(editor) => {
             console.log('Editor is ready to use!', editor);
           }}
@@ -65,7 +80,15 @@ function Editor5({ title, explanation, isButtonVisible, handleButtonClick }) {
           }}
         />
         {isButtonVisible && (
-          <button className="next-button" onClick={() => handleButtonClick()}>
+          <button
+            className="next-button"
+            onClick={() => {
+              if (editorContent.length >= 20) {
+                handleButtonClick();
+              }
+            }}
+            disabled={editorContent.length < 20} // 텍스트 길이가 20 미만이면 버튼 비활성화
+          >
             Next
           </button>
         )}
