c69475f765d8da7ba1f5b6db4ca81e8cffe1cb60
YongILTree1 <kyi4698@LAPTOP-8U5AUD4I>
2023-08-13 10:49:02 +0900

    header 작업중 2

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -5,23 +5,53 @@ const StyledHeader = styled.header`
   padding: 0;
   position: fixed;
   box-sizing: border-box;
+  min-width: auto;
+  width: 100%;
+  height: 56px;
   display: flex;
-  border-top: 5px solid;
+  justify-content: center;
+  border-top: 3px solid rgba(255, 153, 0, 1);
+  border-bottom: 1px solid rgba(214, 217, 220, 1);
+
+  .header-container {
+    width: 97.2307692rem;
+    max-width: 100%;
+    height: 100%;
+    display: flex;
+    align-items: center;
+    margin: 0 auto;
+  }
+
+  .logo-box {
+  }
+
+  .searchbar {
+    display: flex;
+    align-items: center;
+    padding-left: 10px;
+    border: 1px solid rgba(186, 191, 196, 1);
+    border-radius: 10px;
+    flex-grow: 1;
+    flex-shrink: 10000;
+  }
 `;
 
 export default function Header() {
   return (
     <StyledHeader>
-      <div>로고</div>
-      <div>About</div>
-      <div>Product</div>
-      <div>For Teams</div>
-      <div>
-        <input type="text" />
-      </div>
-      <div>
-        <div>Log in</div>
-        <div>Sign up</div>
+      <div className="header-container">
+        <div className="logo-box">로고</div>
+        <div>About</div>
+        <div>Product</div>
+        <div>For Teams</div>
+        <form className="searchbar">
+          <img src="/search-icon.png" alt="search-icon" />
+          <input type="text" />
+        </form>
+        <div>
+          <div>Log in</div>
+          <div>Sign up</div>
+        </div>
       </div>
     </StyledHeader>
   );
