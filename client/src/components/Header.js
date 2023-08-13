ae71d4c1da8e1ad8338705dbffd0a171131c1672
YongILTree1 <kyi4698@LAPTOP-8U5AUD4I>
2023-08-13 23:37:21 +0900

    header 스타일링 작업 중

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -23,6 +23,31 @@ const StyledHeader = styled.header`
   }
 
   .logo-box {
+    display: flex;
+    align-items: center;
+    height: 100%;
+    padding: 0 8px;
+
+    span {
+      color: black;
+    }
+
+    b {
+      color: black;
+    }
+  }
+
+  .logo-box img {
+    height: 30px;
+    margin: 0 3px;
+  }
+
+  .header-container nav {
+    display: flex;
+
+    a {
+      color: black;
+    }
   }
 
   .searchbar {
@@ -34,24 +59,77 @@ const StyledHeader = styled.header`
     flex-grow: 1;
     flex-shrink: 10000;
   }
+
+  .membership-button-box {
+    display: flex;
+    align-items: center;
+    padding-right: 12px;
+    height: 100%;
+  }
+
+  .membership-button-box a {
+    font-size: 13px;
+    text-align: center;
+    position: relative;
+    padding: 8px 0.8em;
+    margin-left: auto;
+    cursor: pointer;
+  }
+
+  .login-button {
+    background-color: rgba(225, 236, 244, 1);
+    border-radius: 5px;
+    color: rgba(57, 115, 157, 1);
+  }
+
+  .signup-button {
+    background-color: rgba(10, 149, 255, 1);
+    border-radius: 5px;
+    color: white;
+  }
+`;
+
+const StyledOl = styled.ol`
+  list-style: none;
+  display: flex;
 `;
 
 export default function Header() {
   return (
     <StyledHeader>
       <div className="header-container">
-        <div className="logo-box">로고</div>
-        <div>About</div>
-        <div>Product</div>
-        <div>For Teams</div>
+        <a href="/" className="logo-box">
+          <img src="/small-logo.png" alt="logo-img" />
+          <span>Stack</span>
+          <b>overflow</b>
+        </a>
+        <StyledOl>
+          <nav className="small-nav">
+            <a href="https://stackoverflow.co/">
+              <div>About</div>
+            </a>
+            <div>Product</div>
+            <a href="https://stackoverflow.co/teams/">
+              <div>For Teams</div>
+            </a>
+          </nav>
+        </StyledOl>
         <form className="searchbar">
           <img src="/search-icon.png" alt="search-icon" />
           <input type="text" />
         </form>
-        <div>
-          <div>Log in</div>
-          <div>Sign up</div>
-        </div>
+        <StyledOl className="membership-button-box">
+          <li>
+            <a href="/login" className="login-button">
+              Log in
+            </a>
+          </li>
+          <li>
+            <a href="/signup" className="signup-button">
+              Sign up
+            </a>
+          </li>
+        </StyledOl>
       </div>
     </StyledHeader>
   );
