c77d5d9682bff10cdcc65c4b2d8ab9948a40db05
YongILTree1 <kyi4698@LAPTOP-8U5AUD4I>
2023-08-14 14:57:39 +0900

    footer 작업중

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import Header from './components/Header';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <>
       <Header />
       <div className="App"></div>
+      <Footer />
     </>
   );
 }
--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -4,12 +4,16 @@ const StyledHeader = styled.header`
   margin: 0;
   padding: 0;
   position: fixed;
+  top: 0;
+  left: 0;
+  z-index: 999;
   box-sizing: border-box;
   min-width: auto;
   width: 100%;
   height: 56px;
   display: flex;
   justify-content: center;
+  background-color: white;
   border-top: 3px solid rgba(255, 153, 0, 1);
   border-bottom: 1px solid rgba(214, 217, 220, 1);
 
@@ -42,22 +46,45 @@ const StyledHeader = styled.header`
     margin: 0 3px;
   }
 
-  .header-container nav {
-    display: flex;
-
-    a {
-      color: black;
-    }
-  }
-
   .searchbar {
     display: flex;
     align-items: center;
     padding-left: 10px;
-    border: 1px solid rgba(186, 191, 196, 1);
-    border-radius: 10px;
     flex-grow: 1;
     flex-shrink: 10000;
+
+    div {
+      display: flex;
+      align-items: center;
+      flex-grow: 1;
+      position: relative;
+      padding: 0 8px;
+    }
+
+    input {
+      border: 1px solid rgba(186, 191, 196, 1);
+      border-radius: 5px;
+      display: block;
+      width: 100%;
+      cursor: unset;
+      padding: 0.6em 0.7em 0.6em 32px;
+      color: rgba(12, 13, 14, 1);
+      font-size: 13px;
+
+      ::placeholder {
+        color: rgba(196, 191, 196, 1);
+      }
+    }
+
+    img {
+      width: 18px;
+      height: 18px;
+      color: rgba(131, 140, 149, 1);
+      left: 0.7em;
+      right: auto;
+      position: absolute;
+      pointer-events: none;
+    }
   }
 
   .membership-button-box {
@@ -89,9 +116,36 @@ const StyledHeader = styled.header`
   }
 `;
 
+const Styelnav = styled.nav`
+  height: 100%;
+  display: flex;
+  align-items: center;
+`;
+
 const StyledOl = styled.ol`
   list-style: none;
   display: flex;
+  gap: 4px;
+  padding: 2px 0;
+
+  .small-nav {
+    display: flex;
+    align-items: center;
+    :hover {
+      background-color: rgba(227, 230, 232, 1);
+      color: rgba(35, 38, 41, 1);
+      border-radius: 1000px;
+    }
+    a {
+      color: rgba(82, 89, 96, 1);
+      padding: 6px 12px;
+    }
+    span {
+      cursor: pointer;
+      color: rgba(82, 89, 96, 1);
+      padding: 6px 12px;
+    }
+  }
 `;
 
 export default function Header() {
@@ -104,32 +158,36 @@ export default function Header() {
           <b>overflow</b>
         </a>
         <StyledOl>
-          <nav className="small-nav">
-            <a href="https://stackoverflow.co/">
-              <div>About</div>
-            </a>
-            <div>Product</div>
-            <a href="https://stackoverflow.co/teams/">
-              <div>For Teams</div>
-            </a>
-          </nav>
+          <li className="small-nav">
+            <a href="https://stackoverflow.co/">About</a>
+          </li>
+          <li className="small-nav">
+            <span>Product</span>
+          </li>
+          <li className="small-nav">
+            <a href="https://stackoverflow.co/teams/">For Teams</a>
+          </li>
         </StyledOl>
         <form className="searchbar">
-          <img src="/search-icon.png" alt="search-icon" />
-          <input type="text" />
+          <div>
+            <img src="/search-icon.png" alt="search-icon" />
+            <input type="text" placeholder="Search..." />
+          </div>
         </form>
-        <StyledOl className="membership-button-box">
-          <li>
-            <a href="/login" className="login-button">
-              Log in
-            </a>
-          </li>
-          <li>
-            <a href="/signup" className="signup-button">
-              Sign up
-            </a>
-          </li>
-        </StyledOl>
+        <Styelnav>
+          <StyledOl className="membership-button-box">
+            <li>
+              <a href="/login" className="login-button">
+                Log in
+              </a>
+            </li>
+            <li>
+              <a href="/signup" className="signup-button">
+                Sign up
+              </a>
+            </li>
+          </StyledOl>
+        </Styelnav>
       </div>
     </StyledHeader>
   );
