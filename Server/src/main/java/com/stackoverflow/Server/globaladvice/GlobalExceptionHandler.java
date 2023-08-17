d29ee6a19796d0b27015efd55065afadf4f4aa17
yawn <yawning055@gmail.com>
2023-08-17 14:58:09 +0900

    오류메시지 일단 여기까지 마무리 했습니다..

--- Server/src/main/java/com/stackoverflow/Server/globaladvice/GlobalExceptionHandler.java
+++ Server/src/main/java/com/stackoverflow/Server/globaladvice/GlobalExceptionHandler.java
@@ -1,16 +1,31 @@
 package com.stackoverflow.Server.globaladvice;
 
+import com.stackoverflow.Server.exception.BusinessLogicException;
+import com.stackoverflow.Server.response.ErrorResponse;
 import org.hibernate.exception.ConstraintViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
     @ExceptionHandler
     public ResponseEntity ConstraintViolationExceptionHandler(ConstraintViolationException e) {
-        System.out.println("오류 잡기 성공");
-        return new ResponseEntity("오류잡기 성공", HttpStatus.BAD_REQUEST);
+        List<String> response = new ArrayList<>();
+        response.add(e.getCause().toString());
+        return new ResponseEntity(response, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler
+    public ResponseEntity BusinessLogicException(BusinessLogicException e) {
+        String response = e.getMessage();
+        return new ResponseEntity(response, HttpStatus.BAD_REQUEST);
     }
 }
