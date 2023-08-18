0e09d25bdcdfeb384a2acc81e866e63440bbaff8
yawn <yawning055@gmail.com>
2023-08-18 11:55:48 +0900

    SecurityConfiguration @CrossOrigin 붙였습니다..!

--- Server/src/main/java/com/stackoverflow/Server/config/SecurityConfiguration.java
+++ Server/src/main/java/com/stackoverflow/Server/config/SecurityConfiguration.java
@@ -6,8 +6,10 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 @Configuration
+@CrossOrigin
 public class SecurityConfiguration {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
